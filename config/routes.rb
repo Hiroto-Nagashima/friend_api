015550ca69e7bb938905bee3dbd93fbe4db91f95
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-08-07 01:05:41 +0900

    [#2][update]ルーティングに名前空間適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :create, :destroy, :update]
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users, only: [:show, :create, :destroy, :update]
+    end
+  end
 end
