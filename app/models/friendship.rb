eb77a7727cb984f504c3c709efe9c637650b8b9b
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-08-06 10:34:19 +0900

    [#1][add]モデルのアソシエーションとバリデーション設定

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :friend_id, presence: true
+  validates :user_id, uniqueness: { scope: :friend_id }
 end
