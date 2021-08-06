52cbb042fd66760190af0817ec401871f12b9f8a
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-08-07 00:19:51 +0900

    [#1][update]Friendshipモデルのバリデーション更新

--- app/validator/friendship_validator.rb
+++ app/validator/friendship_validator.rb
@@ -1,7 +1,7 @@
 class FriendshipValidator < ActiveModel::Validator
   def validate(record)
     if record.user_id == record.friend_id
-      record.errors.add :user_id, '自分を友達に登録できません'
+      record.errors.add :user_id, 'User and friend must be different'
     end
 
     if User.find_by(id: record.friend_id) == nil && record.friend_id != nil
